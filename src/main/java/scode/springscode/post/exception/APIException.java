f5731de4b8d125c8ed0b3356ac89fdee79884949
jung <hjs7949@naver.com>
2023-12-12 23:31:19 +0900

    post w1

---
+++ src/main/java/scode/springscode/post/exception/APIException.java
@@ -0,0 +1,7 @@
+package scode.springscode.post.exception;
+
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@
+public class APIException extends ResponseEntityExceptionHandler {
+}
