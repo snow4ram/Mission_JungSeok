7d1a4fec4df9717ca4ad924db846a08aabd4417a
jung <hjs7949@naver.com>
2023-12-12 16:56:24 +0900

    w1

---
+++ src/main/java/scode/springscode/SpringSCodeApplication.java
@@ -0,0 +1,13 @@
+package scode.springscode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSCodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/scode/springscode/SpringSCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package scode.springscode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
